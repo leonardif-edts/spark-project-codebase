30d97f125b56ccba02eceb62f4159d978194ed7e
leonardif-edts <leonardi.fabianto@sg-edts.com>
2022-12-13 10:19:02 +0700

    services - restructure scripts

--- local_spark.sh
+++ local_spark.sh
@@ -1,18 +1,23 @@
-# Spark - Standalone Script
+# Spark - Local Script
 # 
-# Deployment and usage guide for standalone Spark in Docker
+# Deployment and usage guide for Local Spark in Docker
+# Run each command separately to see each stage of deployment/testing.
 
 # Build Image
 # pattern: docker build -t IMAGE_NAME DOCKERFILE_DIR
-docker build -t spark-standalone:0.1 ./spark
+docker build -t spark-standalone:0.1 ./services/spark
 
 # Run Container
 # pattern: docker run --name CONTAINER_NAME -d IMAGE_NAME sleep infinity
-docker run --name spark-standalone -d spark-standalone:0.1 sleep infinity
+docker run --name spark-standalone -v ${pwd}/project:/opt/bitnami/spark/work/project -d spark-standalone:0.1 sleep infinity
 
 # Run Sample Job
 # pattern: docker exec CONTAINER_NAME bash -c '${SPARK_HOME}/bin/pyspark project/runner.py JOB_NAME ARGS*'
 docker exec spark-standalone bash -c '${SPARK_HOME}/bin/spark-submit project/runner.py example_data_flattening --test'
+
+# Check Data Result
+# pattern: docker exec CONTAINER_NAME bash -c 'ls tmp/result/JOB_NAME-result' 
+docker exec spark-standalone bash -c 'ls tmp/result/example-data-flattening-result'
 
 # Stop Container
 # pattern: docker container stop CONTAINER_NAME
